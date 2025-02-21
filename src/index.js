df039d31802b96cd3128dc55e8ce809faef9628d
leeyoonseo <diano679@gmail.com>
2025-02-22 01:47:11 +0900

    천 단위로 콤마 찍기 과제 이윤서 사원 제출

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 export function toLocaleString(num) {
-  const text = "test문자001122334455문.자,특수기호##!"
+  const text = "test문자001122334455문.자,특수기호##!";
 
-  let number = Number(text.replaceAll(/[^0-9]/g, ""))
-  let answer = number.toLocaleString('ko-KR')
+  // 방법 1
+  let num = Number(text.replaceAll(/[^0-9]/g, ""));
+  let answer = num.toLocaleString('ko-KR');
+
+  // 방법 2
+  // let num2 = text.replaceAll(/[^0-9]/g, "");
+  // let answer2 = num.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return answer;
 }
